e8581fee8648923a1a525d395a17ef3cd80039aa
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-10-05 16:42:03 +0900

    Button: Create Button Component
    See: Github issue #14

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,29 @@
+// This Component is Skeleton of React Structure for Web Development
+// If you want to make other Component, Copy and Refactor this Component.
+
+import React, { Component } from "react";
+import cx from "classnames";
+import * as styles from "./Button.scss";
+
+const defaultProps = {};
+const propTypes = {};
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { children, className, onClick } = this.props;
+    return (
+      <button className={cx(styles.button, className)} onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+}
+
+Button.defaultProps = defaultProps;
+Button.propTypes = propTypes;
+
+export default Button;
