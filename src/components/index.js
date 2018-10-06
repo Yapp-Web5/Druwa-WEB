38081b36a50fe7bc32d15667946de1e541ec0a2a
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-10-06 21:39:56 +0900

    Card: Create Card Component
    See: Github issue #18

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,17 @@
 import Button from "./Button/Button";
+import Card from "./Card/Card";
 import Futura from "./Futura/Futura";
 import Highlight from "./Highlight/Highlight";
 import NavigationBar from "./NavigationBar/NavigationBar";
 import PdfViewer from "./PdfViewer/PdfViewer";
 import PdfElement from "./PdfViewer/PdfElement";
 
-export { Button, Futura, Highlight, NavigationBar, PdfViewer, PdfElement };
+export {
+  Button,
+  Card,
+  Futura,
+  Highlight,
+  NavigationBar,
+  PdfViewer,
+  PdfElement,
+};
