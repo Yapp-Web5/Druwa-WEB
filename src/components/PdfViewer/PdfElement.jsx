24178d12b03aba454b601c56bdc477f8677aa97d
AnGwangHo <akh4444@nate.com>
2018-11-22 01:14:48 +0900

    PdfElement.jsx : 크롬 확장기능 사용하여 우회
    See: Github issue #23

--- src/components/PdfViewer/PdfElement.jsx
+++ src/components/PdfViewer/PdfElement.jsx
@@ -4,16 +4,16 @@ import * as styles from "./PdfElement.scss";
 
 const PdfElement = ({ file, pageNumber, onDocumentLoad, width }) => {
   //cros처리
-  const cros_file = {
-    url: file,
-    httpHeaders: { "X-CustomHeader": "40359820958024350238508234" },
-    withCredentials: true,
-  };
+  // const cros_file = {
+  //   url: file,
+  //   httpHeaders: { "X-CustomHeader": "40359820958024350238508234" },
+  //   withCredentials: true,
+  // };
 
   return (
     <Document
       className={styles.pdfElement}
-      file={cros_file}
+      file={file}
       onLoadSuccess={onDocumentLoad}
     >
       <Page
