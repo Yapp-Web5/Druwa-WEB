3e024f21de50611f08e2e9520df546a43856504c
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-11-23 01:20:55 +0900

    Update src/views/RoomView/RoomView.jsx
    some method 사용하여 Logic 수정
    
    Co-Authored-By: AnGwangHo <akh4444@nate.com>

--- src/views/RoomView/RoomView.jsx
+++ src/views/RoomView/RoomView.jsx
@@ -132,7 +132,13 @@ class RoomView extends Component {
     this.props.createCard(room.url, cardInput, 1);
   };
 
-  isAdmin = () => {
+isAdmin = () => {
+    const { me, room } = this.props;
+    if (room) {
+      return room.admins.some(admin => admin._id === me._id)
+    }
+    return false;
+  }
     const { room } = this.props;
     let ret = false;
 
